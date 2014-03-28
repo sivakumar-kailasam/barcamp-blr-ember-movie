796ff61644bb0d80421eba47fe8b229900e6074d
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2014-03-29 01:39:06 +0530

    added movie route

--- js/app.js
+++ js/app.js
@@ -3,22 +3,19 @@ var movies = [{
 	"name": "captain_america_the_winter_soldier",
 	"title": "Captain America: The Winter Soldier",
 	"synopsis": "Steve Rogers continues his journey as the super-powered American soldier who's grasping to find his place in a modern world after being frozen in ice since WWII with this Marvel Studios sequel. Chris Evans returns to star, with Community director/producers Joe and Anthony Russo helming.",
-	"thumbnail": "http://content8.flixster.com/movie/11/17/42/11174290_det.jpg",
-	"pics": ["http://content8.flixster.com/rtmovie/11/41/114178_gal.jpg", "http://content7.flixster.com/rtmovie/11/36/113637_gal.jpg", "http://content6.flixster.com/rtmovie/11/36/113636_gal.jpg"]
+	"thumbnail": "http://content8.flixster.com/movie/11/17/42/11174290_det.jpg"
 }, {
 	"id": "1",
 	"name": "how_to_train_your_dragon_2",
 	"title": "How to Train Your Dragon 2",
 	"synopsis": "DreamWorks Animation returns to the world of dragons and Vikings in this sequel to their successful 2010 outing How to Train Your Dragon. The original film followed the exploits of a Viking chief's son, who must capture a dragon in order to mark his passage into manhood and prove his worthiness to the tribe",
-	"thumbnail": "http://content7.flixster.com/movie/11/17/49/11174941_det.jpg",
-	"pics": ["http://content7.flixster.com/rtmovie/11/42/114229_gal.jpg", "http://content7.flixster.com/rtmovie/11/35/113509_gal.jpg", "http://content8.flixster.com/rtmovie/11/42/114230_gal.jpg"]
+	"thumbnail": "http://content7.flixster.com/movie/11/17/49/11174941_det.jpg"
 }, {
 	"id": "2",
 	"name": "x_men_days_of_future_past",
 	"title": "X-Men: Days of Future Past",
 	"synopsis": "The ultimate X-Men ensemble fights a war for the survival of the species across two time periods. The characters from the original 'X-Men' film trilogy join forces with their younger selves from the past, in order to change a major historical event and fight in an epic battle that could save our future. ",
-	"thumbnail": "http://content7.flixster.com/movie/11/17/70/11177049_det.jpg",
-	"pics": ["http://content9.flixster.com/rtmovie/11/59/115975_gal.jpg", "http://content8.flixster.com/rtmovie/11/59/115974_gal.jpg", "http://content7.flixster.com/rtmovie/11/47/114765_gal.jpg"]
+	"thumbnail": "http://content7.flixster.com/movie/11/17/70/11177049_det.jpg"
 }];
 
 
@@ -54,5 +51,11 @@ App.IndexRoute = Ember.Route.extend({
 App.MoviesIndexRoute = Ember.Route.extend({
 	model: function() {
 		return movies;	
+	}
+});
+
+App.MovieRoute = Ember.Route.extend({
+	model: function(params) {
+		return movies.findBy('name', params.movie_name);	
 	}
 });
